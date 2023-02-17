d4e7eb3f33ae6dc88e841d9dcff1c6864a4c76fb
jordan smith <jordan.smith6035@gmail.com>
2023-02-17 14:56:20 -0700

    Refactor/fix api calls with return

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,9 +1,9 @@
-export const getApiData = () => {
+export const getApiData: any = () => {
     const category:string = 'general'
     const limit:number = 20
     const apiKey:string = 'Tlpiy7d9s9RpAkcN1ZI/mw==w80MkFur50vVZGkJ'
 
-    fetch(`https://api.api-ninjas.com/v1/trivia?category=${category}&limit=${limit}`, {
+    return fetch(`https://api.api-ninjas.com/v1/trivia?category=${category}&limit=${limit}`, {
     headers: {
         'X-Api-Key': apiKey,
         'Content-Type': 'application/json'
