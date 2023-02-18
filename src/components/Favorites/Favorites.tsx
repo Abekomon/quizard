1c264112dc473abe8c55af21b54ca3f962663dab
Max Lange <max@langes.us>
2023-02-18 14:30:20 -0500

    Set up Favorites component

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import Question from '../Question/Question'
 import './Favorites.css'
 
-const Favorites = () => {
+const Favorites: React.FunctionComponent<any> = props => {
+  const cards: Array<any> = props.questions.map((
+    item: {
+      question: string,
+      answer: string
+    }, index: number) => {
+    return (
+      <Question
+      question={item.question}
+      answer={item.answer}
+      id={Date.now()}
+      key={index} 
+      />
+    )
+  })
+
   return(
-    <>
-    </>
+    <div className='favorites-container'>
+      {cards}
+    </div>
   )
 }
 
