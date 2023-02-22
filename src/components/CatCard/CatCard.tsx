41eea55bc4327ec34d7aa2473891328b978c75b1
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-22 14:42:17 -0700

    Add function props

--- src/components/CatCard/CatCard.tsx
+++ src/components/CatCard/CatCard.tsx
@@ -3,12 +3,13 @@ import './CatCard.css'
 interface CatCardProps {
     category: string,
     title: string,
-    image: string
+    image: string,
+    APICall: React.FunctionComponent
 }
 
 const CatCard: React.FunctionComponent<any> = (props: CatCardProps) => {
     return (
-        <div className='cat-wrapper'>
+        <div className='cat-wrapper' onClick={() => props.APICall(props.category)}>
             <img src={props.image} />
             <h2>{props.title}</h2>
         </div>
