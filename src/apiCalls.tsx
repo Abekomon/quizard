f26bff6857634ca5837261f0a11731ba3238fba3
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-22 15:31:00 -0700

    Add method to API call

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -3,6 +3,7 @@ export const getApiData: any = (category: string) => {
     const apiKey:string = 'Tlpiy7d9s9RpAkcN1ZI/mw==w80MkFur50vVZGkJ'
 
     return fetch(`https://api.api-ninjas.com/v1/trivia?category=${category}&limit=20`, {
+    method: "GET",
     headers: {
         'X-Api-Key': apiKey,
         'Content-Type': 'application/json'
