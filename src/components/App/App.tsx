1ac080bf58d85abfe20365b3dd589951f753135f
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-22 14:41:48 -0700

    Add category API call function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -28,8 +28,24 @@ class App extends React.Component<{}, {quizData: any[], favorites:any, error: ob
     this.setState({favorites: filterFavs})
   }
 
-  componentDidMount() {
-    getApiData("general").then((data: Array<any>) => {
+  // componentDidMount() {
+  //   getApiData("general").then((data: Array<any>) => {
+  //     const mappedData = data.map((item, index) => {
+  //       return {
+  //         id: index,
+  //         ...item
+  //       }
+  //     })
+  //     this.setState({quizData: mappedData})
+  //   })
+  //   .catch((error: object) => {
+  //     this.setState({error:error})
+  //   })
+  // }
+
+  categoryAPICall = (endpoint: string) => {
+    console.log('Anything')
+    getApiData(endpoint).then((data: Array<any>) => {
       const mappedData = data.map((item, index) => {
         return {
           id: index,
@@ -48,7 +64,7 @@ class App extends React.Component<{}, {quizData: any[], favorites:any, error: ob
       <main className='app-container'>
         <Link to='/'><h1 className='heading'>Quizard</h1></Link> 
         <Switch>
-          <Route exact path='/' render={ () => <Categories /> }/>
+          <Route exact path='/' render={ () => <Categories categoryAPICall={this.categoryAPICall} /> }/>
           <Route exact path='/quiz' render={ () => <Quiz questions={this.state.quizData} addFavorite={this.addFavorite} /> } />
           <Route exact path='/favorites' render={ () => <Favorites questions={this.state.favorites} deleteFav={this.deleteFavorite}/> } />
         </Switch>
