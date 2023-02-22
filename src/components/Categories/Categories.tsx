ca7b57dccf7668e374603b162e3c4fa71d37abae
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-22 15:33:23 -0700

    Add component did mount

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,49 +1,63 @@
 import './Categories.css'
 import CatCard from '../CatCard/CatCard'
+import { Component } from 'react'
 
-const Categories: React.FunctionComponent<any> = ({categoryAPICall}) => {
+// const Categories: React.FunctionComponent<any> = ({categoryAPICall}) => {
 
-    return (
-        <div className='cat-container'>
-            <CatCard 
-            category="general"
-            title={"General"}
-            image=""
-            APICall={categoryAPICall}
-            />
-            <CatCard 
-            category="music"
-            title="Music"
-            image=""
-            APICall={categoryAPICall}
-            />
-            <CatCard 
-            category="entertainment"
-            title="Entertainment"
-            image=""
-            APICall={categoryAPICall}
-            />
-            <CatCard 
-            category="sciencenature"
-            title="Science & Nature"
-            image=""
-            APICall={categoryAPICall}
-            />
-            <CatCard 
-            category="sportsleisure"
-            title="Sports & Leisure"
-            image=""
-            APICall={categoryAPICall}
-            />
-            <CatCard 
-            category="historyholidays"
-            title="History Holidays"
-            image=""
-            APICall={categoryAPICall}
-            />
-        </div>
-    )
+class Categories extends Component<any> {
+    constructor(props: any) {
+        super(props)
+    }
+
+componentDidMount = () => {
+    this.props.clearData()
 }
+
+render() {
+return (
+    <div className='cat-container'>
+        <CatCard 
+        category="general"
+        title={"General"}
+        image=""
+        APICall={this.props.categoryAPICall}
+        />
+        <CatCard 
+        category="music"
+        title="Music"
+        image=""
+        APICall={this.props.categoryAPICall}
+        />
+        <CatCard 
+        category="entertainment"
+        title="Entertainment"
+        image=""
+        APICall={this.props.categoryAPICall}
+        />
+        <CatCard 
+        category="sciencenature"
+        title="Science & Nature"
+        image=""
+        APICall={this.props.categoryAPICall}
+        />
+        <CatCard 
+        category="sportsleisure"
+        title="Sports & Leisure"
+        image=""
+        APICall={this.props.categoryAPICall}
+        />
+        <CatCard 
+        category="historyholidays"
+        title="History Holidays"
+        image=""
+        APICall={this.props.categoryAPICall}
+        />
+    </div>
+)
+    }
+}
+
+
 
 
 export default Categories
