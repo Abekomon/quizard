357de2432797faab3dd55f8b3460600fd2c0907f
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-02-23 17:14:41 -0700

    Add images to category cards

--- src/components/CatCard/CatCard.tsx
+++ src/components/CatCard/CatCard.tsx
@@ -11,9 +11,7 @@ interface CatCardProps {
 const CatCard: React.FunctionComponent<any> = (props: CatCardProps) => {
     return (
         <Link to='/quiz' className='cat-link'>
-            <div className='cat-wrapper' onClick={() => props.APICall(props.category)}>
-                <img src={props.image} />
-                <h2>{props.title}</h2>
+            <div className={`cat-wrapper cat-${props.category}`} onClick={() => props.APICall(props.category)}>
             </div>
         </Link>
     )
--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -18,38 +18,26 @@ return (
     <div className='cat-container'>
         <CatCard 
         category="general"
-        title={"General"}
-        image=""
         APICall={this.props.categoryAPICall}
         />
         <CatCard 
         category="music"
-        title="Music"
-        image=""
         APICall={this.props.categoryAPICall}
         />
         <CatCard 
         category="entertainment"
-        title="Entertainment"
-        image=""
         APICall={this.props.categoryAPICall}
         />
         <CatCard 
         category="sciencenature"
-        title="Science & Nature"
-        image=""
         APICall={this.props.categoryAPICall}
         />
         <CatCard 
         category="sportsleisure"
-        title="Sports & Leisure"
-        image=""
         APICall={this.props.categoryAPICall}
         />
         <CatCard 
         category="historyholidays"
-        title="History Holidays"
-        image=""
         APICall={this.props.categoryAPICall}
         />
     </div>
